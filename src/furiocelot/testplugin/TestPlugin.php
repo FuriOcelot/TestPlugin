bc2ccdd68536ee5c6af642b14b031575ebc8dc86
FuriousOcelot <msroo3721@gmail.com>
2019-06-06 10:28:09 +0900

    namespace aliasing 테스트 https://www.php.net/manual/en/language.namespaces.importing.php

--- src/furiocelot/testplugin/TestPlugin.php
+++ src/furiocelot/testplugin/TestPlugin.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 namespace furiocelot\testplugin;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as Color;
 
 class TestPlugin extends PluginBase{
 	public function onLoad() : void{
-		$this->getLogger()->info(TextFormat::WHITE . "I've been loaded!");
+		$this->getLogger()->info(Color::WHITE . "I've been loaded!");
 	}
 
 	public function onEnable() : void{
-		$this->getLogger()->info(TextFormat::DARK_GREEN . "I've been enabled!");
+		$this->getLogger()->info(Color::DARK_GREEN . "I've been enabled!");
 	}
 
 	public function onDisable() : void{
-		$this->getLogger()->info(TextFormat::DARK_RED . "I've been disabled!");
+		$this->getLogger()->info(Color::DARK_RED . "I've been disabled!");
 	}
 }
