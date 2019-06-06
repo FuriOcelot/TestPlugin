65ff3053921af5f0dc528bbd14dbfbe2c92b14c7
FuriousOcelot <msroo3721@gmail.com>
2019-06-06 10:59:27 +0900

    테스트 코드 정리

--- src/furiocelot/testplugin/TestPlugin.php
+++ src/furiocelot/testplugin/TestPlugin.php
@@ -11,13 +11,7 @@ use pocketmine\{
 class TestPlugin extends PluginBase{
 	public function onLoad() : void{
 		$this->getLogger()->info(Color::WHITE . "I've been loaded!");
-
-		$a = "banana";
-		$b = "apple";
-		$this->getLogger()->info(Color::YELLOW . "\$a=>$a, \$b=>$b");
-		$this->swap($a, $b);
-		$this->getLogger()->info(Color::YELLOW . "\$a=>$a, \$b=>$b");
-	}
+}
 
 	public function onEnable() : void{
 		$this->getLogger()->info(Color::DARK_GREEN . "I've been enabled!");
@@ -25,9 +19,5 @@ class TestPlugin extends PluginBase{
 
 	public function onDisable() : void{
 		$this->getLogger()->info(Color::DARK_RED . "I've been disabled!");
-	}
-
-	private function swap(&$a, &$b) : void{
-		[$a, $b] = [$b, $a];
 	}
 }
